e5a5f6c9647317994dd8e1d4970b8ecca345bce8
Héloïse Le Lez <lelez.heloise@gmail.com>
2021-11-29 11:56:30 +0100

    Ex1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//        Menu.DisplayMenu();
+        Prix.Prix(100, 0.5F, 0.3f);
 
     }
+
 }
